98c87e7dd71f2deb6d12b2cd4a17a40b38cdb206
9ties <griwvctoa987@gmail.com>
2024-09-04 22:15:18 +0200

    Change Big

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+$( document ).ready(function() {
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY == 0) {
+            $('#myVideo2').addClass('visibility');
+            $('#myVideo2').get(0).currentTime = 0;
+            console.log("hey")
+        }
+    
+        if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
+            document.getElementById('myVideo2').play();
+            $('#myVideo2').removeClass('visibility');
+            //document.getElementById('myVideo2').style.visibility = true;
+        }
+    });
+
+});
+
